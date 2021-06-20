39977ae5b3109bc9f816578ffce1dfa703e1d73e
Tae.Garam <drasla@naver.com>
2021-06-20 21:38:17 +0900

    # 10.1 Login UI Clone

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,19 +1,148 @@
 import {darkModeVar, isLoggedInVar} from "../apollo";
 import styled from "styled-components";
+import {
+    faFacebook,
+    faFacebookF,
+    faFacebookSquare,
+    faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Title = styled.h1`
   color: ${(props) => props.theme.fontColor};
   background-color: ${(props) => props.theme.bgColor};
 `;
 
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  
+  height: 100vh;
+`;
+
+const WhiteBox = styled.div`
+  background-color: white;
+  border: 1px solid #dbdbdb;
+  width: 100%;
+`;
+
+const TopBox = styled(WhiteBox)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 35px 40px 25px 40px;
+  margin-bottom: 10px;
+  
+  form {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    
+    margin-top: 35px;
+    width: 100%;
+    
+    input {
+      width: 100%;
+      border-radius: 3px;
+      padding: 7px;
+      background-color: #fafafa;
+      border: 0.5px solid #dbdbdb;
+      margin-top: 5px;
+      box-sizing: border-box;
+      
+      &::placeholder {
+        font-size: 12px;
+      }
+      &:last-child {
+        border: none;
+        margin-top: 12px;
+        background-color: #0095f6;
+        color: white;
+        text-align: center;
+        padding: 8px 0px;
+        
+        font-weight: 600;
+      }
+    }
+  }
+`;
+
+const BottomBox = styled(WhiteBox)`
+  padding: 10px 40px;
+  text-align: center;
+  
+  a {
+    font-weight: 600;
+    color: #0095f6;
+  }
+`;
+
+const Wrapper = styled.div`
+  max-width: 350px;
+  width: 100%;
+`;
+
+const Separator = styled.div`
+  margin: 20px 0px 30px 0px;
+  text-transform: uppercase;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  
+  div {
+    width: 100%;
+    height: 1px;
+    background-color: #dbdbdb;
+  }
+  
+  span {
+    margin: 0px 10px;
+    font-weight: 600;
+    color: #8e8e8e;
+  }
+`;
+
+const FacebookLogin = styled.div`
+  color: #385285;
+  span {
+    margin-left: 10px;
+    font-weight: 600;
+  }
+`;
+
 const Login = () => {
     return (
-        <>
-            <Title>Login</Title>
-            <button onClick={() => isLoggedInVar(true)}>Log in now!</button>
-            <button onClick={() => darkModeVar(true)}>To Dark</button>
-            <button onClick={() => darkModeVar(false)}>To Light</button>
-        </>
+        <Container>
+            <Wrapper>
+                <TopBox>
+                    <div>
+                        <FontAwesomeIcon icon={faInstagram} size="3x" />
+                    </div>
+                    <form>
+                        <input type="text" placeholder="Username" />
+                        <input type="password" placeholder="Password" />
+                        <input type="submit" placeholder="Log in" />
+                    </form>
+                    <Separator>
+                        <div></div>
+                        <span>Or</span>
+                        <div></div>
+                    </Separator>
+                    <FacebookLogin>
+                        <FontAwesomeIcon icon={faFacebookSquare} />
+                        <span>Log in with facebook</span>
+                    </FacebookLogin>
+                </TopBox>
+                <BottomBox>
+                    <span>Don't have an account?</span> <a href="#">Sign up</a>
+                </BottomBox>
+            </Wrapper>
+        </Container>
     );
 
 }
