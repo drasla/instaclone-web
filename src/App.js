8ac9ad1b836ef5bcd6b63163e8d4ef47f0510b0d
Tae.Garam <drasla@naver.com>
2021-06-20 18:42:54 +0900

    # 8.5 Auth POC

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
 
 function App() {
-    const isLoggedIn = false;
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     return (
         <div className="App">
             <Router>
                 <Switch>
                     <Route path="/" exact>
-                        {isLoggedIn ? <Home /> : <Login />}
+                        {isLoggedIn ? <Home setIsLoggedIn={setIsLoggedIn} /> : <Login setIsLoggedIn={setIsLoggedIn} />}
                     </Route>
                     <Route>
                         <NotFound />
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,10 @@
-function Home() {
-    return <h1>Home</h1>;
+function Home({setIsLoggedIn}) {
+    return (
+        <>
+            <h1>Home</h1>
+            <button onClick={setIsLoggedIn(false)}>Log Out</button>
+        </>
+    );
 }
 
 export default Home;
