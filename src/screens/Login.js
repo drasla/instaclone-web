223dcb3a899386b53d937bc5e141c6525f103e10
Tae.Garam <drasla@naver.com>
2021-06-20 19:23:08 +0900

    # 8.8 Themes on Styled Components

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,10 +1,18 @@
-import {isLoggedInVar} from "../apollo";
+import {darkModeVar, isLoggedInVar} from "../apollo";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.fontColor};
+  background-color: ${(props) => props.theme.bgColor};
+`;
 
 const Login = () => {
     return (
         <>
-            <h1>Login</h1>
+            <Title>Login</Title>
             <button onClick={() => isLoggedInVar(true)}>Log in now!</button>
+            <button onClick={() => darkModeVar(true)}>To Dark</button>
+            <button onClick={() => darkModeVar(false)}>To Light</button>
         </>
     );
 
