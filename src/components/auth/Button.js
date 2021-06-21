53fca9f0a719e8713be0032ba251e08c1565c34c
Tae.Garam <drasla@naver.com>
2021-06-21 17:21:41 +0900

    # 10.8 React Hook Form is Awesome

--- src/components/auth/Button.js
+++ src/components/auth/Button.js
@@ -11,6 +11,7 @@ const Button = styled.input`
   padding: 8px 0px;
 
   font-weight: 600;
+  opacity: ${props => props.disabled ? "0.5" : "1"}
 `;
 
 export default Button;
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -13,6 +13,7 @@ import {BottomBox} from "../components/auth/BottomBox";
 import routes from "../routes";
 import PageTitle from "../components/auth/pageTitle";
 import {useForm} from "react-hook-form";
+import FormError from "../components/auth/FormError";
 
 const FacebookLogin = styled.div`
   color: #385285;
@@ -24,7 +25,9 @@ const FacebookLogin = styled.div`
 `;
 
 const Login = () => {
-    const { register, watch, handleSubmit } = useForm();
+    const { register, handleSubmit, errors, formState } = useForm({
+        mode: "onChange"
+    });
     const onSubmitValid = (data) => {
 
     };
@@ -39,16 +42,26 @@ const Login = () => {
                 <div style={{marginBottom: 30}}>
                     <FontAwesomeIcon icon={faInstagram} size="3x"/>
                 </div>
-                <form onSubmit={handleSubmit(onSubmitValid, onSubmitInvalid)}>
-                    <Input ref={register({
-                        required: "Username is required.",
-                        minLength: 5,
-                        validate: (currentValue) => currentValue.includes("")
-                    })} name="username" type="text"  placeholder="Username" />
+                <form onSubmit={handleSubmit(onSubmitValid)}>
+                    <Input
+                        ref={register({
+                            required: "Username is required",
+                            minLength: {
+                                value: 5,
+                                message: "Username should be longer than 5 chars.",
+                            },
+                        })}
+                        name="username"
+                        type="text"
+                        placeholder="Username"
+                        hasError={Boolean(errors?.username?.message)}
+                    />
+                    <FormError message={errors?.username?.message} />
                     <Input ref={register({
                         required: "Password is required."
                     })} name="password" type="password" placeholder="Password" />
-                    <Button type="submit" placeholder="Log in"/>
+                    <FormError message={errors?.password?.message} />
+                    <Button type="submit" placeholder="Log in" disabled={!formState.isValid}/>
                 </form>
                 <Separator>
                     <div></div>
